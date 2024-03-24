87897ee443073f502db24d9ccbbbb75b4f3a33f2
Praveen Kumar <praveenku32k@gmail.com>
2024-03-24 08:30:16 +0530

    update setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Praveen Kumar',
+    author_email= 'praveenku32k2gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
